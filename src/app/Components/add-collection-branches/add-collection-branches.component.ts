43011344d09fe696916c53b11186b2e488f40b8b
Sondos1233 <69219643+Sondos1233@users.noreply.github.com>
2022-03-01 00:17:08 +0200

    add component

--- src/app/Components/add-collection-branches/add-collection-branches.component.ts
+++ src/app/Components/add-collection-branches/add-collection-branches.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-collection-branches',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-collection-branches.component.css']
 })
 export class AddCollectionBranchesComponent implements OnInit {
+  RegisterForm: FormGroup;
+  constructor(private FormService: FormBuilder,
+    private router: Router,
+    private activeRoute: ActivatedRoute) { 
 
-  constructor() { }
+      this.RegisterForm = this.FormService.group({
+        LocName:['', [Validators.required, Validators.minLength(4)]],
+        Address: ['', [Validators.required, Validators.minLength(10)]],
+        Workinghours:['',[Validators.required,Validators.minLength(12)]]
+      });
+
+  }
 
   ngOnInit(): void {
+  }
+  get LocName(){
+    return this.RegisterForm.get('LocName');
+  }
+  get Address(){
+    return this.RegisterForm.get('Address');
+  }
+  get Workinghours(){
+    return this.RegisterForm.get('Workinghours');
   }
 
 }
