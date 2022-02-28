858fb5e3e887b91bfc41c0dd6f481fdaf5d8047f
Sondos1233 <69219643+Sondos1233@users.noreply.github.com>
2022-02-28 23:44:14 +0200

    add component

---
+++ src/app/Components/add-collection-branches/add-collection-branches.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-collection-branches',
+  templateUrl: './add-collection-branches.component.html',
+  styleUrls: ['./add-collection-branches.component.css']
+})
+export class AddCollectionBranchesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,32 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { NavbarComponent } from './Components/navbar/navbar.component'
-import { FooterComponent } from './Components/footer/footer.component';
-
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from "@angular/fire"; 
 import { AngularFirestoreModule , AngularFirestore } from "@angular/fire/firestore";
 import { AngularFireStorageModule, BUCKET  } from "@angular/fire/storage";
-import { ConfirmRestComponent } from './Components/confirm-rest/confirm-rest.component';
-import {RestaurantsComponent} from './Components/restaurants/restaurants.component'
-import { TableComponent } from './Components/table/table.component'
+
+
 import { RestaurantService } from './Service/restaurant.service';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {MatDialogModule} from '@angular/material/dialog';
-import { CustomDialogComponent } from './Components/custom-dialog/custom-dialog.component';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { AddCollectionBranchesComponent } from './Components/add-collection-branches/add-collection-branches.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    RestaurantsComponent,
-    ConfirmRestComponent,
-    TableComponent,
-    CustomDialogComponent,
+    AddCollectionBranchesComponent,
+  
   ],
   imports: [
     BrowserModule,
@@ -39,8 +28,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     BrowserAnimationsModule,
     AngularFireStorageModule,
     FontAwesomeModule,
-    MatDialogModule,
-    MatSnackBarModule
+  
   ],
   providers: [
     RestaurantService,
