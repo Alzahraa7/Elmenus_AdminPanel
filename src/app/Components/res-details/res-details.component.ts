c6b146e4efd30827e1d650b8a2fbef9f2c11cd06
zahra <alzahraam209@gmail.com>
2022-03-01 04:02:32 +0200

    Restaurant details added

---
+++ src/app/Components/res-details/res-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-res-details',
+  templateUrl: './res-details.component.html',
+  styleUrls: ['./res-details.component.css']
+})
+export class ResDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
