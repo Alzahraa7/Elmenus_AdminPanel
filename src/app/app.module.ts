a9c3a2c24c28e62ed9bd990865e8709db6593081
Sondos1233 <69219643+Sondos1233@users.noreply.github.com>
2022-03-01 22:38:31 +0200

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,46 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { environment } from 'src/environments/environment';
-import { AngularFireModule } from "@angular/fire"; 
-import { AngularFirestoreModule , AngularFirestore } from "@angular/fire/firestore";
-import { AngularFireStorageModule, BUCKET  } from "@angular/fire/storage";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
-
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { AddCollectionBranchesComponent } from './Components/add-collection-branches/add-collection-branches.component';
 import { AddCollectionOffersComponent } from './Components/add-collection-offers/add-collection-offers.component';
 import { AddMealComponent } from './Components/add-meal/add-meal.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     AddCollectionBranchesComponent,
     AddCollectionOffersComponent,
-    AddMealComponent,
-  
+    AddMealComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    BrowserAnimationsModule,
-    AngularFireStorageModule,
     FontAwesomeModule,
     FormsModule,
     ReactiveFormsModule,
-  
   ],
-  providers: [
-
-    AngularFirestore,
-   
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
