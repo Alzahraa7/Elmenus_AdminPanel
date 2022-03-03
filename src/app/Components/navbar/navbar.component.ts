89b7d6914492f3d8f055906d9b5c885aa9f58cc9
Rami-Ayyad <engramiahmad97@gmail.com>
2022-03-03 05:26:56 +0200

    Side Bar Finished <3

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SideBarService } from 'src/app/Service/side-bar.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,20 +7,7 @@ import { SideBarService } from 'src/app/Service/side-bar.service';
 })
 export class NavbarComponent implements OnInit {
 
-                  //SIDE BAR SERVICE
-  constructor(private sidebar:SideBarService) { }
-  
-
-  //SIDE BAR FUNCTION
-  toggleActive:boolean = false;
-
-	toggleSideBar() {
-		this.toggleActive = !this.toggleActive;
-		this.sidebar.toggle();
-
-    console.log('Clicked');
-	}
-	
+  constructor() { }
 
   ngOnInit(): void {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {SideBarComponent} from './Components/side-bar/side-bar.component'
+import { SideBarComponent } from './Components/side-bar/side-bar.component'
 import { NavbarComponent } from './Components/navbar/navbar.component'
 import { FooterComponent } from './Components/footer/footer.component';
 
@@ -12,7 +12,6 @@ import { environment } from 'src/environments/environment';
 import { AngularFireModule } from "@angular/fire"; 
 import { AngularFirestoreModule , AngularFirestore } from "@angular/fire/firestore";
 import { AngularFireStorageModule, BUCKET  } from "@angular/fire/storage";
-import { ConfirmRestComponent } from './Components/confirm-rest/confirm-rest.component';
 import { RestaurantsComponent } from './Components/restaurants/restaurants.component'
 import { TableComponent } from './Components/table/table.component'
 import { RestaurantService } from './Service/restaurant.service';
@@ -25,7 +24,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { SideBarService } from './Service/side-bar.service';
+import { ConfirmRestComponent } from './Components/confirm-rest/confirm-rest.component';
 
 
 
@@ -36,10 +35,10 @@ import { SideBarService } from './Service/side-bar.service';
     NavbarComponent,
     FooterComponent,
     RestaurantsComponent,
-    ConfirmRestComponent,
     TableComponent,
     CustomDialogComponent,
     SideBarComponent,
+    ConfirmRestComponent,
 
   
   ],
@@ -63,7 +62,6 @@ import { SideBarService } from './Service/side-bar.service';
   providers: [
     RestaurantService,
     AngularFirestore,
-    SideBarService,
   ],
   bootstrap: [AppComponent]
 })
