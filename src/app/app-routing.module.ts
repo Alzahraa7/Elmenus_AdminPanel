05f6a9705ed01169edd03d2ecd20ec4c4b36cd54
zahra <alzahraam209@gmail.com>
2022-03-04 03:30:31 +0200

    changes will be removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { AddCareerComponent } from './Components/add-career/add-career.component
 import { AddJobComponent } from './Components/add-job/add-job.component';
 import { JobDetailsComponent } from './Components/job-details/job-details.component';
 import { ListCareerComponent } from './Components/list-career/list-career.component';
+import { RestaurantsComponent } from './Components/restaurants/restaurants.component';
 
 const routes: Routes = [
 
   {path:'Careers', component:ListCareerComponent},
   {path:'addAdmin/:name',component:AdminsAddComponent},
   {path:'JobDetails/:Name',component:JobDetailsComponent},
+  {path:'Restaurants',component:RestaurantsComponent},
   {path:'addCareer',component:AddCareerComponent},
   {path:'addCareer/:id',component:AddCareerComponent},
   {path:'addJob/:NameJob',component:AddJobComponent},
